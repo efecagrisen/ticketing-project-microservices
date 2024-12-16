c1e4d9365e985e50c58c815677d520829e9f7617
efecagrisen <efecagrisen@yahoo.com>
2024-12-16 16:05:21 +0300

    added eureka configuration to project-service and task-service

--- task-service/src/main/java/com/cydeo/TaskServiceApplication.java
+++ task-service/src/main/java/com/cydeo/TaskServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
